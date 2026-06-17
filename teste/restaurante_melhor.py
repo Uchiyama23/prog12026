be2a0b125c7b8fc077a1648eb562e451d8ac9c3c
Uchiyama23 <antoniamartinsphilippcarneiro@gmail.com>
2026-06-17 10:11:46 -0300

    Create restaurante_melhor.py

---
+++ teste/restaurante_melhor.py
@@ -0,0 +1,26 @@
+from datetime import date
+class cliente:
+    def __init__(self, nome:str, data_nascimento: date, cpf:str):
+        self.nome= nome
+        self.data_nascimento= data_nascimento
+        self.cpf= cpf
+
+class pedido:
+    def __init__(self, data_pedido:date, percentual_desconto:float, valor_final:float, cliente:cliente, itens:list):
+        self.data_pedido=data_pedido
+        self.percentual_desconto= percentual_desconto
+        self.valor_final= valor_final
+        self.cliente= cliente
+        self.itens= itens
+class prato:
+    def __init__ (self, nome:str, ingredientes:list[str], modo_preparo:str, preco:float):
+        self.nome=nome
+        self.ingredientes=ingredientes
+        self.modo_preparo=modo_preparo
+        self.preco=preco
+
+class itemdopedido:
+    def __init__(self, prato:prato, valor_prato:str, quantidade:int):
+        self.prato=prato
+        self.valor_prato=valor_prato
+        self.quantidade=quantidade
