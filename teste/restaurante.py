1d59afe2aef768fa55104a1bfd1b67a857a4a937
Uchiyama23 <antoniamartinsphilippcarneiro@gmail.com>
2026-06-17 10:03:51 -0300

    Update restaurante.py

--- teste/restaurante.py
+++ teste/restaurante.py
@@ -6,4 +6,16 @@ class cliente:
         self.cpf= cpf
 
 class pedido:
-    def __init__(self, )
+    def __init__(self, data_pedido:date, percentual_desconto:float, valor_final:float, cliente:cliente, pratos:list):
+        self.data_pedido=data_pedido
+        self.percentual_desconto= percentual_desconto
+        self.valor_final= valor_final
+        self.cliente= cliente
+        self.pratos= pratos
+
+class prato:
+    def __init__ (self, nome:str, ingredientes:list[str], modo_preparo:str, preco:float):
+        self.nome=nome
+        self.ingredientes=ingredientes
+        self.modo_preparo=modo_preparo
+        self.preco=preco
