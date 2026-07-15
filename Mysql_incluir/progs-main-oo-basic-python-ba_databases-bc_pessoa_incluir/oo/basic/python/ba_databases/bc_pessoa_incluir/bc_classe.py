059e77bbec4a6ec560e3c495f54740c4d939e79b
Uchi <antoniamartinsphilippcarneiro@gmail.com>
2026-07-15 13:44:43 -0300

    “incluir”

---
+++ Mysql_incluir/progs-main-oo-basic-python-ba_databases-bc_pessoa_incluir/oo/basic/python/ba_databases/bc_pessoa_incluir/bc_classe.py
@@ -0,0 +1,4 @@
+# definição da classe Pessoa
+class Pessoa:
+    def __init__(self, nome:str):
+        self.nome = nome
---
+++ Mysql_incluir/progs-main-oo-basic-python-ba_databases-bc_pessoa_incluir/oo/basic/python/ba_databases/bc_pessoa_incluir/bd_teste.py
@@ -0,0 +1,7 @@
+# importar a classe
+from bc_classe import Pessoa
+
+# criar uma nova pessoa
+nova = Pessoa("Joao da Silva")
+# exibir os dados
+print(nova.nome, nova.email, nova.telefone)
